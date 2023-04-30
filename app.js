a71be2923845bae23d7eb203c624693df5726ff1
Sungho <hurgj123kr@gmail.com>
2023-04-30 22:02:29 +0900

    fake db users create

--- app.js
+++ app.js
@@ -4,9 +4,29 @@ const port = 3000;
 
 const app = express()
 
+let users = [
+    {
+        id: 1,
+        name: "Joy"
+    },
+    {
+        id: 2,
+        name: "Bob"
+    },
+    {
+        id: 3,
+        name: "James",
+    },
+    {
+        id: 4,
+        name: "god",
+    },
+]
+
 app.get("/", (req, res) => {
-  res.send("Hello world!")
+    res.send("Hello world!")
 })
+app.get("/users", (req, res) => res.json(users));
 const handleAppListen = () => console.log(`✅ http://localhost:${port}/`)
 
 app.listen(port, handleAppListen);
