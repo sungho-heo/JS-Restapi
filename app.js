2d8c5614054bd77fd48f5a952abccdc9c2e51e88
Sungho <hurgj123kr@gmail.com>
2023-05-01 13:14:08 +0900

    server users id query add

--- app.js
+++ app.js
@@ -23,10 +23,22 @@ let users = [
     },
 ]
 
+app.get("/users", (req, res) => res.json(users));
+app.get("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    if (!id) {
+        return res.status(400).json({ error: "Incorrect id" });
+    }
+    let user = users.filter(user => user.id === id)[0];
+    if (!user) {
+        return res.status(404).json({ error: "Unknown user" });
+    };
+    return res.json(user);
+});
 app.get("/", (req, res) => {
     res.send("Hello world!")
 })
-app.get("/users", (req, res) => res.json(users));
+
 const handleAppListen = () => console.log(`✅ http://localhost:${port}/`)
 
 app.listen(port, handleAppListen);
