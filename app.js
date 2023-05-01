ca1de30e8a380aa5f5166b8257b033709aaa3c0b
Sungho <hurgj123kr@gmail.com>
2023-05-01 18:41:49 +0900

    server parser add and post user api add but not create

--- app.js
+++ app.js
@@ -23,22 +23,45 @@ let users = [
     },
 ]
 
-app.get("/users", (req, res) => res.json(users));
-app.get("/users/:id", (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    if (!id) {
-        return res.status(400).json({ error: "Incorrect id" });
-    }
-    let user = users.filter(user => user.id === id)[0];
-    if (!user) {
-        return res.status(404).json({ error: "Unknown user" });
-    };
-    return res.json(user);
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //body-parser에서 body data use config.
 app.get("/", (req, res) => {
     res.send("Hello world!")
 })
 
+app.get("/users", (req, res) => res.json(users))
+
+app.get("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10)
+  if (!id) {
+    return res.status(400).json({ error: "Incorrect id" })
+  }
+  let user = users.filter((user) => user.id === id)[0]
+  if (!user) {
+    return res.status(404).json({ error: "Unknown user" })
+  }
+  return res.json(user)
+})
+
+app.delete("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10)
+  if (!id) {
+    return res.status(400).json({ error: "Incorrect id" })
+  }
+  let userIdx = users.findIndex((userId) => userId.id === id)
+  if (userIdx === -1) {
+    return res.status(404).json({ error: "Unkwon user" })
+  }
+  users.splice(userIdx, 1)
+  return res.status(204).send()
+})
+
+app.post("/users", (req, res) => {
+    const name = req.body.name || "";
+    if (!name.length) {
+        return res.status(400).json({ error: "Plz Update user name" });
+    } 
+})
 const handleAppListen = () => console.log(`✅ http://localhost:${port}/`)
 
 app.listen(port, handleAppListen);
