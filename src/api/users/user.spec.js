4f281212b16312bebd9ad956e8a63f7ca68ee6b0
Sungho <hurgj123kr@gmail.com>
2023-05-06 19:32:15 +0900

    a little update

--- src/api/users/user.spec.js
+++ src/api/users/user.spec.js
@@ -18,57 +18,27 @@ describe("GET /users", () => {
     User.bulkCreate(users).then(() => done());
   });
   it("should return array", (done) => {
-    request(app)
-      .get("/users")
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err
-        res.body.should.be.an.instanceof(Array).and.have.length(3)
-        res.body.map((user) => {
-          user.should.have.properties("id", "name")
-          user.id.should.be.a.Number()
-          user.name.should.be.a.String()
-        });
-        done();
+    request(app).get("/users").expect(500).end((err, res) => {
+      if (err) throw err;
+      res.body.should.be.instanceof(Array).and.have.length(4)
+      res.body.map(user => {
+        user.should.have.properties("id", "name");
+        user.id.should.be.a.Number();
+        user.name.should.be.a.String();
       });
+      done();
+    });
   });
   after("clear up database", (done) => {
     dbReset().then(() => done());
   });
 });
 
-// describe("GET /users", () => {
-//   before("sync database", async () => {
-//     await sequelize.sync({ force: true }).then(() => {
-//       console.log("Database sync");
-//     })
-//   });
-//   it("should return 200 status code", (done) => {
-//     request(app).get("/users").expect(200).end((err, res) =>{
-//       if (err) throw err;
-//       done()
-//     });
-//   });
-
-//   it("should return array", (done) => {
-//     request(app).get("/users").expect(200).end((err, res) =>{
-//       if (err) throw err;
-//       res.body.should.be.an.instanceof(Array).and.have.length(4);
-//       res.body.map(user =>{
-//         user.should.have.properties("id","name");
-//         user.id.should.be.a.Number();
-//         user.name.should.be.a.String();
-//       });
-//       done();
-//     });
-//   });
-// });
-
-// describe("PUT /users/:id", () => {
-//   it.only("should return 200 status code", (done) => {
-//     request(app).put("/users/1").send({ name: "puddle" }).end((err, res) => {
-//       if (err) throw err;
-//       done();
-//     });
-//   });
-// });
+describe("PUT /users/:id", () => {
+  it.only("should return 200 status code", (done) => {
+    request(app).put("/users/1").send({ name: "puddle" }).end((err, res) => {
+      if (err) throw err;
+      done();
+    });
+  });
+});
